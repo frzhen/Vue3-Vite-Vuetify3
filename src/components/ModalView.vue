1fbf04e25cf3a95bd3b1dd806b46474147dce425
Fred Zhen <13911239075@163.com>
2022-02-15 00:34:23 +0800

    Add Vite migration link to README.md, delete the index.html inside public folder, add index.html at root directory.

---
+++ src/components/ModalView.vue
@@ -0,0 +1,58 @@
+<!--
+  * @Author: Fred R. Zhen
+  * @Date: 2022/2/5 12:59
+  * @Email: fred.zhen@gmail.com
+-->
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-btn
+          class="mt-16"
+          color="primary"
+          dark
+          @click.stop="dialog = true">
+        Modal Example
+      </v-btn>
+      <v-dialog
+          v-modal="dialog"
+          max-width="300">
+        <v-card>
+          <v-card-title class="text-h5">
+            Modal Example
+          </v-card-title>
+          <v-card-text>
+            Suns wobble on moon at captain's quarters!
+          </v-card-text>
+          <v-card-actions>
+            <v-btn
+                color="green darken-1"
+                text
+                @click="dialog = false">
+              Close
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
+  </v-container>
+
+</template>
+
+<script>
+import Vuetify from "@/plugins/vuetify";
+
+export default {
+  name: "ModalView",
+  vuetify:new Vuetify(),
+  data() {
+    return {
+      dialog: false,
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app-bar>
+    <v-toolbar-title class="headline text-uppercase">
+      <span>Vuetify</span>
+      <span class="font-weight-light">Material Design</span>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn
+        flat
+        target="_blank"
+    >
+      <span class="mr-2"><a :href="vuetifyUrl">Latest Release</a></span>
+    </v-btn>
+  </v-app-bar>
+  <v-content>
+    <HelloWorld msg="Welcome to Your Vue.js App with Vuetify and Vite"/>
+  </v-content>
+
 </template>
 
 <script>
@@ -13,6 +26,11 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data: () => {
+    return {
+      vuetifyUrl: 'https://github.com/vuetifyjs/vuetify/releases/tag/v3.0.0-alpha.12',
+    }
   }
 }
 </script>
